7eaa7785e294af29b04a414e1b48a3ea4b6a7154
Shingo <kawanos@google.com>
2021-06-16 19:46:37 +0900

    modify

--- main.go
+++ main.go
@@ -10,21 +10,31 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 var initV string
+var Hostname string
 
 type slackResult struct {
 	Message string `json:"message"`
 	Status  string `json:"status"`
 }
 
+func postForm(message string) (result []byte, err error) {
+	d := time.Now()
+	newmessage := fmt.Sprintf("%s(%s:%s)", message, Hostname, d.String())
+	resp, _ := http.PostForm("https://api.uname.link/slack", url.Values{"message": {newmessage}})
+	// result, err := ioutil.ReadAll(resp.Body)
+	return ioutil.ReadAll(resp.Body)
+}
+
 func init() {
 	if initV == "" {
-		resp, _ := http.PostForm("https://api.uname.link/slack", url.Values{"message": {"init"}})
-		result, _ := ioutil.ReadAll(resp.Body)
+		Hostname, _ = os.Hostname()
+		result, _ := postForm("init")
 		var s slackResult
 		log.Printf("result: %v\n", string(result))
 		json.Unmarshal(result, &s)
@@ -37,7 +47,7 @@ func init() {
 	go func() {
 		sig := <-sigs
 		t := fmt.Sprintf("%s", sig)
-		http.PostForm("https://api.uname.link/slack", url.Values{"message": {"sigterm:" + t}})
+		postForm("signal:" + t)
 	}()
 }
 
