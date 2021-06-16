7a5747fa2a8b5af7ad6d77d73fbad89a560230e9
Shingo <kawanos@google.com>
2021-06-16 19:12:29 +0900

    change

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -14,11 +16,20 @@ import (
 
 var initV string
 
+type slackResult struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
 func init() {
 	if initV == "" {
 		resp, _ := http.PostForm("https://api.uname.link/slack", url.Values{"message": {"init"}})
 		result, _ := ioutil.ReadAll(resp.Body)
-		initV = string(result)
+		var s slackResult
+		log.Printf("result: %v\n", string(result))
+		json.Unmarshal(result, &s)
+		initV = s.Message
+		log.Printf("message: %v\n", initV)
 	}
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGTERM)
