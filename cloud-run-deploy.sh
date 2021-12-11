982d50d729db657f5bdeedc0affc9d710d0e85f0
Shingo <kawanos@google.com>
2021-12-12 00:04:35 +0900

    take optional args

--- cloud-run-deploy.sh
+++ cloud-run-deploy.sh
@@ -2,4 +2,4 @@
 
 DEPLOY=${1:-cloud-run-lifecycle}
 echo $DEPLOY
-gcloud beta run deploy --source=. --platform=managed --region=us-central1 --allow-unauthenticated $DEPLOY
+gcloud beta run deploy --source=. --platform=managed --region=us-central1 --allow-unauthenticated $DEPLOY $@
